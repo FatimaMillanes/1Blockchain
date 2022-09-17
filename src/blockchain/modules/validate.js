cc21d89163f594f620ae318d7596b735483172b6
FatimaMillanes <fatima.rosela@hotmail.com>
2022-09-17 14:25:52 -0700

    validate.test

--- src/blockchain/modules/validate.js
+++ src/blockchain/modules/validate.js
@@ -1,14 +1,18 @@
 import Block from "../block";
 
 export default (blockchain) => {
-    const [genesisBlock, ...blocks] = blockchain
-    if (JSON.stringify(genesisBlock) !== JSON.stringify(Block.genesis)) throw Error('Bloque Genesis Error');
-    for (let i = 0 ; i < blocks.length; i+=1){
+    const [genesisBlock, ...blocks] = blockchain;
+
+    if (JSON.stringify(genesisBlock) !== JSON.stringify(Block.genesis)) throw Error('Bloque genesis invalido');
+
+    for (let i = 0 ; i < blocks.length; i +=1){
         const{
-            previousHash, timestamp, hash, data
-        } = blocks[1];
-        const previousBlock = blockchain[1];
-        if (previousHash !== previousBlock.hash) throw Error('Hash previo invalido o corrupto');
-        if (hash !== Block.hash(timestamp, previousHash, data)) throw Error('Hash invalido');
+            previousHash, timestamp, hash, data, nonce, difficulty,
+        } = blocks[i];
+        const previousBlock = blockchain[i];
+
+        if (previousHash !== previousBlock.hash) throw Error('Hash previo invalido');
+        if (hash !== Block.hash(timestamp, previousHash, data, nonce, difficulty)) throw Error('Hash invalido');
     }
-}
+    return true;
+};
